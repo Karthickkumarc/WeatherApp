5b264bdee7ec06d5ebbaa6e10e808d43e3cf15ae
Karthickkumarc <ckarthickkumar2019gmail.com>
2024-04-03 16:05:56 +0530

    gitweather

---
+++ src/Components/WeatherApp.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import "./WeatherDetails.css";
+
+
+
+import WeatherDetails from './WeatherDetails';
+function WeatherApp() {
+  
+    return (
+        <>
+          
+             <WeatherDetails  />
+           
+         
+        </>
+    )
+}
+export default WeatherApp;
